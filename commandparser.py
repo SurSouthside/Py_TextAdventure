22dfcbf1b580fe150e8ceffdbc09a569b592824c
SurSouthside <Stuart.SouthIII1@gmail.com>
2022-05-30 10:40:16 -0400

    Start work on reading and parsing commands from json object

--- commandparser.py
+++ commandparser.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
+import json
+import os
+
+
 class CommandParser:
 
     def __init__(self):
-        #self._game_status = ''
-        self._valid_exploration_commands = ['help', 'status']
-        self._valid_battle_commands = ['help', 'status']
-        self._valid_shop_commands = ['help', 'status']
+        self._game_status = ''
+        # self._valid_exploration_commands = ['help', 'status']
+        # self._valid_battle_commands = ['help', 'status']
+        # self._valid_shop_commands = ['help', 'status']1
+        self._commands = open(r'D:\Python_Apps\Python_TextAdventure\py_textadventure\assets\command_list.json')
+        self._command_list = json.load(self._commands)
 
     def get_game_status(self):
         return self.game_status
@@ -21,4 +27,7 @@ class CommandParser:
         return ''
 
     def parse_command(self, command, game_status):
+        the_dir = os.path.dirname(__file__)
         print("You entered: " + command)
+        for i in self._command_list['exploration_commands']:
+            print(i)
