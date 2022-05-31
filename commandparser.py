2229e59d0aa3fedf87ef704e6e9f67ca696cb959
SurSouthside <Stuart.SouthIII1@gmail.com>
2022-05-30 23:29:03 -0400

    Get relative path of JSON file for command parsing

--- commandparser.py
+++ commandparser.py
@@ -10,8 +10,12 @@ class CommandParser:
         self._game_status = ''
         # self._valid_exploration_commands = ['help', 'status']
         # self._valid_battle_commands = ['help', 'status']
-        # self._valid_shop_commands = ['help', 'status']1
-        self._commands = open(r'D:\Python_Apps\Python_TextAdventure\py_textadventure\assets\command_list.json')
+        # self._valid_shop_commands = ['help', 'status']
+        # print(__file__)
+        # print(os.path.dirname(__file__))
+        # print(os.path.join(os.path.dirname(__file__), 'assets', 'command_list.json'))
+        # self._commands = open(r'D:\Python_Apps\Python_TextAdventure\py_textadventure\assets\command_list.json')
+        self._commands = open((os.path.join(os.path.dirname(__file__), 'assets', 'command_list.json')))
         self._command_list = json.load(self._commands)
 
     def get_game_status(self):
@@ -27,7 +31,6 @@ class CommandParser:
         return ''
 
     def parse_command(self, command, game_status):
-        the_dir = os.path.dirname(__file__)
         print("You entered: " + command)
         for i in self._command_list['exploration_commands']:
             print(i)
